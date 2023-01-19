3bb197d27be1afb0a6597477a8d0adbc78ebacd0
Stefano Copreni <58907791+Nazckul@users.noreply.github.com>
2023-01-19 18:13:41 -0300

    complete the oauth functionality

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -59,7 +59,7 @@ export default function SignUp() {
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
-            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357"
+            src="https://nuezaweb.com/photo-sign.jpg"
             alt="key"
             className="w-full rounded-2xl"
           />
